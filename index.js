4bd9b41f51a2e5abe5e21ed34ae24fd0398f957b
Francisco <linux.development404@gmail.com>
2020-11-10 16:56:28 -0600

    Agregando coordenadas adicionales

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = [{ lat: 23.1965945, lng: -99.1251677 }, {lat: 19.800212, lng: -90.486852}, {lat: 19.051601, lng: -96.424461}, {lat: 19.5731501, lng: -90.3614499}];
+    const uluru = [{ lat: 23.1965945, lng: -99.1251677 }, {lat: 19.800212, lng: -90.486852}, {lat: 19.051601, lng: -96.424461}, {lat: 19.5731501, lng: -90.3614499}, {lat: 18.082199, lng: -96.116653}];
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,

cf5c3f03360e56ecdcc6ec62a0933c2c57bf8a19
Francisco <linux.development404@gmail.com>
2020-11-10 16:47:23 -0600

    links a perfiles con anchor reparado y en nuevo tab

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = [{ lat: 23.1965945, lng: -99.1251677 }, {lat: 19.800212, lng: -90.486852}, {lat: 19.1, lng: -96.5}];
+    const uluru = [{ lat: 23.1965945, lng: -99.1251677 }, {lat: 19.800212, lng: -90.486852}, {lat: 19.051601, lng: -96.424461}, {lat: 19.5731501, lng: -90.3614499}];
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
